9e8a7929805f96a60a95cc9c393936d8fca7f5fc
Escande Guillaume <escande.guillaume@gmail.com>
2023-11-09 11:28:57 +0100

    Add github action to build doc and push on github pages

--- doc/generate.bash
+++ doc/generate.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+set -e
 # version : 0.1.0
 
 SCRIPT=$(realpath "$0")
@@ -10,4 +12,5 @@ echo "Antora build"
 
 
 docker build ${SCRIPTPATH} --no-cache -t syson_doc_generator:local
+
 docker run -v $SCRIPTPATH/..:/usr/app syson_doc_generator:local
